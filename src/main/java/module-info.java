1339f7515ddd259404a3e019b5ae16f9fa66fccf
Marwan Abdalla <marwan3m2000@gmail.com>
2023-11-23 13:03:08 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module fhtw.chatroom_frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens fhtw.chatroom_frontend to javafx.fxml;
+    exports fhtw.chatroom_frontend;
+}
